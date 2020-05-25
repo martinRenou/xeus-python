08ab71f2cc478192d847fe128fa6140fe9473525
Bartosz Telenczuk <bartosz.telenczuk@gmail.com>
2020-05-25 10:23:35 +0200

    refactor registering magics

--- src/xcomm.cpp
+++ src/xcomm.cpp
@@ -211,35 +211,40 @@ namespace xpyt
                 _magics_module = py::module::import("IPython.core.magics");
                 _extension_module = py::module::import("IPython.core.extensions");
 
-                magics_manager = _magic_core.attr("MagicsManager")();
+                magics_manager = _magic_core.attr("MagicsManager")("shell"_a=this);
                 extension_manager = _extension_module.attr("ExtensionManager")("shell"_a=this);
 
                 //shell features required by extension manager
                 builtin_trap = py::module::import("contextlib").attr("nullcontext")();
                 ipython_dir = "";
 
-                py::object osm_magics_inst =  _magics_module.attr("OSMagics")();
-                py::object basic_magics_inst =  _magics_module.attr("BasicMagics")();
-                py::object user_magics_inst =  _magics_module.attr("UserMagics")();
-                py::object extension_magics_inst =  _magics_module.attr("ExtensionMagics")();
-                magics_manager.attr("user_magics") = user_magics_inst;
-                osm_magics_inst.attr("shell") = this;
-                extension_magics_inst.attr("shell") = this;
-                osm_magics_inst.attr("magics")["cell"] = py::dict(
-                    "writefile"_a=osm_magics_inst.attr("writefile"));
-                osm_magics_inst.attr("magics")["line"] = py::dict(
-                    "cd"_a=osm_magics_inst.attr("cd"),
-                    "env"_a=osm_magics_inst.attr("env"),
-                    "set_env"_a=osm_magics_inst.attr("set_env"),
-                    "pwd"_a=osm_magics_inst.attr("pwd"));
-                basic_magics_inst.attr("shell") = this;
-                basic_magics_inst.attr("magics")["cell"] = py::dict();
-                basic_magics_inst.attr("magics")["line"] = py::dict(
-                    "magic"_a=basic_magics_inst.attr("magic"));
-                magics_manager.attr("register")(osm_magics_inst);
-                magics_manager.attr("register")(basic_magics_inst);
-                magics_manager.attr("register")(user_magics_inst);
-                magics_manager.attr("register")(extension_magics_inst);
+                py::object osm_magics =  _magics_module.attr("OSMagics");
+                py::object basic_magics =  _magics_module.attr("BasicMagics");
+                py::object user_magics =  _magics_module.attr("UserMagics");
+                py::object extension_magics =  _magics_module.attr("ExtensionMagics");
+                magics_manager.attr("register")(osm_magics);
+                magics_manager.attr("register")(basic_magics);
+                magics_manager.attr("register")(user_magics);
+                magics_manager.attr("register")(extension_magics);
+                magics_manager.attr("user_magics") = user_magics("shell"_a=this);
+
+                //select magics supported by xeus-python
+                auto line_magics = magics_manager.attr("magics")["line"];
+                auto cell_magics = magics_manager.attr("magics")["cell"];
+                line_magics = py::dict(
+                   "cd"_a=line_magics["cd"],
+                   "env"_a=line_magics["env"],
+                   "set_env"_a=line_magics["set_env"],
+                   "pwd"_a=line_magics["pwd"],
+                   "magic"_a=line_magics["magic"],
+                   "load_ext"_a=line_magics["load_ext"]
+                );
+                cell_magics = py::dict(
+                    "writefile"_a=cell_magics["writefile"]);
+
+                magics_manager.attr("magics") = py::dict(
+                   "line"_a=line_magics,
+                   "cell"_a=cell_magics);
             }
 
 
