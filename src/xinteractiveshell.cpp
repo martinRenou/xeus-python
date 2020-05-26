cfed12f5bac1196f0a16f88ca93a432bc788557f
Bartosz Telenczuk <bartosz.telenczuk@gmail.com>
2020-05-26 11:53:16 +0200

    hiding readonly attributes

--- src/xinteractiveshell.cpp
+++ src/xinteractiveshell.cpp
@@ -13,26 +13,26 @@ namespace xpyt
         m_magics_module = py::module::import("IPython.core.magics");
         m_extension_module = py::module::import("IPython.core.extensions");
 
-        magics_manager = m_magic_core.attr("MagicsManager")("shell"_a=this);
-        extension_manager = m_extension_module.attr("ExtensionManager")("shell"_a=this);
+        m_magics_manager = m_magic_core.attr("MagicsManager")("shell"_a=this);
+        m_extension_manager = m_extension_module.attr("ExtensionManager")("shell"_a=this);
 
         //shell features required by extension manager
-        builtin_trap = py::module::import("contextlib").attr("nullcontext")();
-        ipython_dir = "";
+        m_builtin_trap = py::module::import("contextlib").attr("nullcontext")();
+        m_ipython_dir = "";
 
         py::object osm_magics =  m_magics_module.attr("OSMagics");
         py::object basic_magics =  m_magics_module.attr("BasicMagics");
         py::object user_magics =  m_magics_module.attr("UserMagics");
         py::object extension_magics =  m_magics_module.attr("ExtensionMagics");
-        magics_manager.attr("register")(osm_magics);
-        magics_manager.attr("register")(basic_magics);
-        magics_manager.attr("register")(user_magics);
-        magics_manager.attr("register")(extension_magics);
-        magics_manager.attr("user_magics") = user_magics("shell"_a=this);
+        m_magics_manager.attr("register")(osm_magics);
+        m_magics_manager.attr("register")(basic_magics);
+        m_magics_manager.attr("register")(user_magics);
+        m_magics_manager.attr("register")(extension_magics);
+        m_magics_manager.attr("user_magics") = user_magics("shell"_a=this);
 
         //select magics supported by xeus-python
-        auto line_magics = magics_manager.attr("magics")["line"];
-        auto cell_magics = magics_manager.attr("magics")["cell"];
+        auto line_magics = m_magics_manager.attr("magics")["line"];
+        auto cell_magics = m_magics_manager.attr("magics")["cell"];
         line_magics = py::dict(
            "cd"_a=line_magics["cd"],
            "env"_a=line_magics["env"],
@@ -44,7 +44,7 @@ namespace xpyt
         cell_magics = py::dict(
             "writefile"_a=cell_magics["writefile"]);
 
-        magics_manager.attr("magics") = py::dict(
+        m_magics_manager.attr("magics") = py::dict(
            "line"_a=line_magics,
            "cell"_a=cell_magics);
     }
@@ -52,10 +52,10 @@ namespace xpyt
 
     xinteractive_shell::xinteractive_shell()
     {
-        hooks = hooks_object();
+        m_hooks = hooks_object();
         m_ipy_process = py::module::import("IPython.utils.process");
-        db = py::dict();
-        user_ns = py::dict("_dh"_a=py::list());
+        m_db = py::dict();
+        m_user_ns = py::dict("_dh"_a=py::list());
         init_magics();
     }
 
@@ -73,7 +73,7 @@ namespace xpyt
     py::object xinteractive_shell::run_line_magic(std::string name, std::string arg)
     {
 
-        py::object magic_method = magics_manager
+        py::object magic_method = m_magics_manager
             .attr("magics")["line"]
             .attr("get")(name);
 
@@ -88,7 +88,7 @@ namespace xpyt
 
     py::object xinteractive_shell::run_cell_magic(std::string name, std::string arg, std::string body)
     {
-        py::object magic_method = magics_manager.attr("magics")["cell"].attr("get")(name);
+        py::object magic_method = m_magics_manager.attr("magics")["cell"].attr("get")(name);
 
         if (magic_method.is_none()) {
             PyErr_SetString(PyExc_ValueError, "cell magics not found");
@@ -100,12 +100,50 @@ namespace xpyt
 
     void xinteractive_shell::register_magic_function(py::object func, std::string magic_kind, py::object magic_name)
     {
-        magics_manager.attr("register_function")(
+        m_magics_manager.attr("register_function")(
             func, "magic_kind"_a=magic_kind, "magic_name"_a=magic_name);
     }
 
     void xinteractive_shell::register_magics(py::args args)
     {
-        magics_manager.attr("register")(*args);
+        m_magics_manager.attr("register")(*args);
     }
+
+    // define getters
+
+    py::object xinteractive_shell::get_magics_manager()
+    {
+        return m_magics_manager;
+    }
+
+    py::object xinteractive_shell::get_extension_manager()
+    {
+        return m_extension_manager;
+    }
+
+    py::dict xinteractive_shell::get_db()
+    {
+        return m_db;
+    }
+
+    py::dict xinteractive_shell::get_user_ns()
+    {
+        return m_user_ns;
+    }
+
+    py::object xinteractive_shell::get_builtin_trap()
+    {
+        return m_builtin_trap;
+    }
+
+    py::str xinteractive_shell::get_ipython_dir()
+    {
+        return m_ipython_dir;
+    }
+
+    hooks_object xinteractive_shell::get_hooks()
+    {
+        return m_hooks;
+    }
+
 }
--- src/xinteractiveshell.hpp
+++ src/xinteractiveshell.hpp
@@ -23,19 +23,11 @@ namespace xpyt
     {
 
     public:
-        //required by cd magic and others
-        py::dict db;
-        py::dict user_ns;
 
-        //required by extension_manager
-        py::object builtin_trap;
-        py::str ipython_dir;
+        // default constructor
+        xinteractive_shell();
 
-        py::object magics_manager;
-        py::object extension_manager;
-
-        hooks_object hooks;
-
+        // mock required methods
         void register_post_execute(py::args, py::kwargs) {};
         void enable_gui(py::args, py::kwargs) {};
         void observe(py::args, py::kwargs) {};
@@ -43,19 +35,26 @@ namespace xpyt
 
         void init_magics();
 
-        xinteractive_shell();
-
+        // run system commands
         py::object system(py::str cmd);
-
         py::object getoutput(py::str cmd); 
 
+        // run magics
         py::object run_line_magic(std::string name, std::string arg);
-
         py::object run_cell_magic(std::string name, std::string arg, std::string body);
          
+        // register magics
         void register_magic_function(py::object func, std::string magic_kind, py::object magic_name);
-
         void register_magics(py::args args);
+
+        // public getters
+        py::object get_magics_manager();
+        py::object get_extension_manager();
+        py::dict get_db();
+        py::dict get_user_ns();
+        py::object get_builtin_trap();
+        py::str get_ipython_dir();
+        hooks_object get_hooks();
 
     private:
         py::module m_ipy_process;
@@ -63,6 +62,18 @@ namespace xpyt
         py::module m_magics_module;
         py::module m_extension_module;
 
+        py::object m_magics_manager;
+        py::object m_extension_manager; // required by %load_ext
+
+        // required by cd magic and others
+        py::dict m_db;
+        py::dict m_user_ns;
+
+        // required by extension_manager
+        py::object m_builtin_trap;
+        py::str m_ipython_dir;
+
+        hooks_object m_hooks;
 
     };
 };
